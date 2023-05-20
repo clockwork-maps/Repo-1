07057ccc7725c3d0a47333fe2d5a9a3a0f7b7c9b
clockwork-maps <131809002+clockwork-maps@users.noreply.github.com>
2023-05-20 13:28:22 +0200

    index mobile pt1
    added some mobile formatting for the main index page. hamburger menu implementation next update.

--- d3js/js/index.js
+++ d3js/js/index.js
@@ -1,12 +1,19 @@
 let preview = d3.selectAll('.preview');
+let mobBool = false;
 preview.each(function(){
     let tsvg = d3.select(this);
     let parent = tsvg.node().parentElement;
     let pbounds = parent.getBoundingClientRect();
     let pmin = Math.min(pbounds.width, pbounds.height)/10;
     parent.style.padding = `${pmin}px`;
-    pmin = Math.min(pbounds.width, pbounds.height)/10;
-    tsvg.style('height', `${8*pmin}px`).style('width', `${12*pmin}px`).style('top', `${pmin*2}px`).style('right', `${pmin}px`);
+    if(pbounds.width > 1000) tsvg.style('height', `${8*pmin}px`).style('width', `${12*pmin}px`).style('top', `${pmin*2}px`).style('right', `${pmin}px`);
+    else{
+        tsvg.style('height', `${8*pmin}px`).style('width', `${pbounds.width-pmin*2}px`).style('bottom', `${pmin}px`);
+        let block = parent.parentElement;
+        block.style.height = `${pbounds.height+13*pmin}px`; 
+        mobBool = true;
+    }
+    
 });
 let eventText = d3.selectAll('.eventText');
 eventText.each(function(){
@@ -14,7 +21,14 @@ eventText.each(function(){
     let parent = ttext.node().parentElement;
     let pbounds = parent.getBoundingClientRect();
     let pmin = Math.min(pbounds.width, pbounds.height)/10;
-    ttext.style('top', `${pmin*.1}px`).style('right',`${pmin*4}px`);
+    if(pbounds.width > 1000) ttext.style('top', `${pmin*.1}px`).style('right',`${pmin*4}px`);
+    else {
+        let svg = parent.querySelector('.preview');
+        let bottom = Number(svg.style.bottom.slice(0,-2));
+        let height = Number(svg.style.height.slice(0,-2));
+        console.log(height + bottom);
+        ttext.style('bottom', `${height+5}px`).style('opacity','1')
+    }
 })
 barChart({target:'#chart1',target2:'bars', dNum:8});
 d3.select('#chart1').on('click', function(event){
@@ -27,10 +41,11 @@ d3.select('#chart3').on('click', function(event){
     event.preventDefault();
     pieChart({target:'#chart3',duration:750, dNum:Math.round(getRA(5,8))});    
 })
-scales({target: '#chart4', target2: 'bAxis', stroke: 'red', posyscalar: .7, posxscalar: .3, tnum: 4, barscalar:.6, dmax: 100});
-scales({target: '#chart4', target2: 'lAxis', stroke: 'red', posyscalar: .2, posxscalar: .3, tnum: 4, barscalar:.5, direction: 'left', dmax: 100});
+let xscalar = mobBool? .2 : .3;
+scales({target: '#chart4', target2: 'bAxis', stroke: 'red', posyscalar: .7, posxscalar: xscalar, tnum: 4, barscalar:.6, dmax: 100});
+scales({target: '#chart4', target2: 'lAxis', stroke: 'red', posyscalar: .2, posxscalar: xscalar, tnum: 4, barscalar:.5, direction: 'left', dmax: 100});
 d3.select('#chart4').on('click', function(event){
     event.preventDefault();
-    scales({target: '#chart4', target2: 'bAxis', stroke: 'red', posyscalar: .7, posxscalar: .3, tnum: 2, barscalar:.6, duration: 750});
-    scales({target: '#chart4', target2: 'lAxis', stroke: 'red', posyscalar: .2, posxscalar: .3, tnum: 2, barscalar:.5, direction: 'left', duration: 750});
+    scales({target: '#chart4', target2: 'bAxis', stroke: 'red', posyscalar: .7, posxscalar: xscalar, tnum: 2, barscalar:.6, duration: 750});
+    scales({target: '#chart4', target2: 'lAxis', stroke: 'red', posyscalar: .2, posxscalar: xscalar, tnum: 2, barscalar:.5, direction: 'left', duration: 750});
 })
