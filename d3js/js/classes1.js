b5fab17607f9d8172702e5a89626d325fea1bbd9
clockwork-maps <131809002+clockwork-maps@users.noreply.github.com>
2023-06-08 12:44:29 +0200

    More mobile fixes
    css and js fixes, html change to display proper pre-formatted code

--- d3js/js/classes1.js
+++ d3js/js/classes1.js
@@ -8,7 +8,6 @@ let axis2 = new AxScale({
     target : 'chart1',
     target2 : 'lAxis2',
     direction :  'left',
-    posyscalar : .1,
     dmax : 50
 })
 axis2.draw();
--- d3js/js/stdClasses.js
+++ d3js/js/stdClasses.js
@@ -4,6 +4,7 @@ class AxScale {
     }    
     static axes = {'bottom':d3.axisBottom, 'left': d3.axisLeft, 'top':d3.axisTop, 'right':d3.axisRight};
     static scales = {'linear':d3.scaleLinear, 'sqrt':d3.scaleSqrt, 'log':d3.scaleLog, 'time':d3.scathisime, 'band':d3.scaleBand, 'point':d3.scalePoint};
+    static posarray = [[.1,.9],[.2,.7]];
     constructor (params){
         this._scaleSelect = params.scale || 'linear';
         this._direction = params.direction || 'bottom';
@@ -15,17 +16,22 @@ class AxScale {
         this._numScale = ['linear','sqrt','log'].indexOf(this._scaleSelect) + 1;
         this._domArray = params.domain || ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
         this._barDomain = this._numScale > 0 ? [this._dmin, this._dmax] : this._domArray;
-        this._barscalar = params.barscalar || .8;
         this._tranD = params.duration || 0;
         this._chart = d3.select(this._target);
         this._ctbounds = this._chart.node().getBoundingClientRect();
         this._ctln = this._ctbounds.width;
         this._ctht = this._ctbounds.height;
+        this._mobile = window.innerWidth < 1024;
+        this._barscalar = params.barscalar || this._mobile && this._hbool ? .6 : this._mobile ? .5 : .8;
         this._hbool = this._direction == 'bottom' || this._direction == 'top';
         this._rmax = this._hbool ? this._ctln*this._barscalar : this._ctht*this._barscalar;
-        this._posx = this._ctln * params.posxscalar || this._ctln * .1;
-        this._posy = this._ctht * params.posyscalar || this._ctht * .9;
-        this._tnum = params.tnum || 10;
+        this._positionBase = this._mobile ? AxScale.posarray[1] : AxScale.posarray[0];
+        this._positions = {'left':[0,0],'top':[0,0],'bottom':[0,1],'right':[1,0]}[this._direction].map(x => this._positionBase[x]);
+        this._posxscalar = params.posxscalar || this._positions[0];
+        this._posyscalar = params.posyscalar || this._positions[1];
+        this._posx = this._ctln * this._posxscalar;
+        this._posy = this._ctht * this._posyscalar;
+        this._tnum = params.tnum || this._mobile ? 3 : 10;
         this._scale = this._hbool ? AxScale.scales[this._scaleSelect]().domain(this._barDomain).range([0, this._rmax]) : AxScale.scales[this._scaleSelect]().domain(this._barDomain.reverse()).range([0, this._rmax]);
         this._axis = AxScale.axes[this._direction](this._scale).ticks(this._tnum);
         this._cscale = !!this._chart.node().querySelector(`.${this._target2}`) ? d3.select(`.${this._target2}`) : this._chart.append('g').classed(this._target2,true);
